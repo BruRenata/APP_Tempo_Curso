ecb972fe4837d05deeb02c15c604ecb054e0f656
whiteRen <brunageratti@hotmail.com>
2022-12-13 22:51:50 -0300

    busca poluição atual

--- src/app/modulos/poluicao/poluicao-home/poluicao-home.component.ts
+++ src/app/modulos/poluicao/poluicao-home/poluicao-home.component.ts
@@ -9,10 +9,26 @@ import { Component } from '@angular/core';
 export class PoluicaoHomeComponent {
 
   displayedColumns: string[] = ['demo-name', 'demo-weight', 'demo-symbol'];
-   polutionData?: PolutionData
+  polutionData?: PolutionData
+  cidadePais: string
+
+  tempoService: TempoService
 
   constructor(tempoService: TempoService) {
-    tempoService.buscarPoluicao(-27.590465150887418, -50.36225636499196).subscribe(data => this.polutionData = data);
+    this.cidadePais = "Correia Pinto, Br";
+    this.tempoService = tempoService;
+    this.buscaPoluicao();
+    //tempoService.buscarTempoAtual("Penha", "Brasil").subscribe(data =>
+      //.buscarPoluicao(data.latitude, data.longitude).subscribe(data => this.polutionData = data)
+    //);
+    //tempoService.buscarPoluicao(-27.5904651508871418, -50.36225636499196).subscribe(data => this.polutionData = data);
+  }
+
+  public buscaPoluicao(): void{
+    this.tempoService.buscarTempoAtual(this.cidadePais.split(",")[0].trim(), this.cidadePais.split(",")[1].trim()).subscribe(data =>
+      this.tempoService.buscarPoluicao(data.latitude, data.longitude).subscribe(data => this.polutionData = data)
+    );
   }
 
 }
+
--- src/app/modulos/poluicao/poluicao.module.ts
+++ src/app/modulos/poluicao/poluicao.module.ts
@@ -1,11 +1,12 @@
-import { AppModule } from './../../app.module';
-import { BuscaCidadeComponent } from 'src/app/busca-cidade/busca-cidade.component';
+//import { AppModule } from './../../app.module';
+//import { BuscaCidadeComponent } from 'src/app/busca-cidade/busca-cidade.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PoluicaoRoutingModule } from './poluicao-routing.module';
 import { PoluicaoHomeComponent } from './poluicao-home/poluicao-home.component';
 import { MaterialModule } from 'src/app/material.module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +16,9 @@ import { MaterialModule } from 'src/app/material.module';
   imports: [
     CommonModule,
     PoluicaoRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
+    //BuscaCidadeComponent
   ]
 })
 export class PoluicaoModule { }
